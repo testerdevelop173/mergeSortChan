91225aa0249a0d2533b123a50d1aeddba5f4d0e7
testerdevelop173 <artemjuravlev232@gmail.com>
2024-12-18 22:26:26 +0300

    two channels are merged, but not sorted, and contain dubls

--- mergeSortChan.go
+++ mergeSortChan.go
@@ -1,6 +1,27 @@
 package main
 
+import "fmt"
+
 func mergeSortedChannels(ch1, ch2 chan int) chan int {
+	outMergChan := make(chan int)
+
+	go func() {
+		defer close(outMergChan)
+
+		go func() {
+			for v := range ch1 {
+				outMergChan <- v
+			}
+		}()
+
+		go func() {
+			for v := range ch2 {
+				outMergChan <- v
+			}
+		}()
+	}()
+
+	return outMergChan
 }
 
 func fillChannel(input []int) chan int {
@@ -17,11 +38,15 @@ func fillChannel(input []int) chan int {
 }
 
 func main() {
-	in1 := []int{1, 3, 4, 5}
-	in2 := []int{-3, 1, 10}
+	in1 := []int{1, 3, 4, 7, 8, 22, 90, 5}
+	in2 := []int{-3, 1, 10, 1, 12, 12, 90}
 
 	ch1 := fillChannel(in1)
 	ch2 := fillChannel(in2)
-	outMergChan := mergeSortedChannels(ch1, ch2)
+
+	fmt.Println("the result is:", mergeSortedChannels(ch1, ch2))
 
 }
+
+// требуется: объединить 2 канала ch1, ch2 и
+//убрать дублирование. Чтобы каждый элемнт был уникальным
